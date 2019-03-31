695c79fca79c9c160671d7e60aa45bca48fd9b92
Gunama Putra <gunama12@gmail.com>
2019-03-31 14:10:19 +0700

    Add table

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 class App extends Component {
   render() {
@@ -20,16 +39,36 @@ class App extends Component {
           			</Typography>
           			<Button color="inherit">Dashboard</Button>
         		</Toolbar>
-      		</AppBar>
-        		
+      		</AppBar>        		
         		<Grid item xs={12} container justify="center" direction="column" className={classes.paper}>
         			<Typography variant="h5" component="h3">
-          			Dashboard
-        		</Typography>
-        		<Typography component="p">
-          			Dashboard Page
-        		</Typography>
-        		</Grid>
+          				Dashboard
+        			</Typography>
+        			<Table className={classes.table}>
+        				<TableHead>
+          					<TableRow>
+            					<TableCell>Dessert (100g serving)</TableCell>
+            					<TableCell align="right">Calories</TableCell>
+            					<TableCell align="right">Fat (g)</TableCell>
+            					<TableCell align="right">Carbs (g)</TableCell>
+            					<TableCell align="right">Protein (g)</TableCell>
+          					</TableRow>
+        				</TableHead>
+        				<TableBody>
+				        {rows.map(row => (
+				            <TableRow key={row.id}>
+				              	<TableCell component="th" scope="row">
+				                	{row.name}
+				              	</TableCell>
+				              	<TableCell align="right">{row.calories}</TableCell>
+				              	<TableCell align="right">{row.fat}</TableCell>
+				              	<TableCell align="right">{row.carbs}</TableCell>
+				              	<TableCell align="right">{row.protein}</TableCell>
+				            </TableRow>
+				        ))}
+				        </TableBody>
+				    </Table>
+				</Grid>
     	</div>
     );
   }
